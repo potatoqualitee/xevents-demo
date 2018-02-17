f6186523f05673431a814c3d7b9a97fbaa3ce1af
Chrissy LeMaire <clemaire@gmail.com>
2018-02-18 00:37:56 +0100

    Add emphasis

--- 3-faster-trace-setup.ps1
+++ 3-faster-trace-setup.ps1
@@ -2,7 +2,7 @@
 
 # Complaint: Faster to setup quick traces
 # Complaint: Templates work remotely across all instances
-# Answer: Now you can easily deploy all of your session templates
+# Answer: Now you can easily deploy all of your session templates SUCH AS LONG RUNNING QUERIES
 $servers = "localhost\sql2016","localhost\sql2017"
 Get-DbaXESessionTemplate | Out-GridView -PassThru | Import-DbaXESessionTemplate -SqlInstance $servers | Start-DbaXESession
 
