fe43c2c42f5646b0591bebcc49466b982cea10d2
Chrissy LeMaire <clemaire@gmail.com>
2018-02-19 23:20:11 +0100

    updates

--- 10-perfmon-mix.ps1
+++ 10-perfmon-mix.ps1
@@ -1,6 +1,10 @@
 ﻿break
 # Complaint: Ability to import PerfMon data and look at Trace and PerfMon counter data at the same time
 
+# Xevents CSV
+# Perfmon CSV
+# Mash them together
+
 # First we enabled exporting to CSV from command line
 Get-ChildItem -Path C:\github\xevents-demo\sample.xel | Export-DbaXECsv -Path c:\temp\sample.csv
 Invoke-Item c:\temp\sample.csv
@@ -21,12 +25,11 @@ Get-DbaPfDataCollectorSetTemplate | Out-GridView -PassThru | Import-DbaPfDataCol
 
 
 # We also made it easy to export perfmon to CSV so you can easily use Power BI
-Get-DbaPfDataCollectorSet -CollectorSet 'PAL - SQL Server 2014 and Up' | Invoke-DbaPfRelog | Update-DbaPowerBiDataSource
-Get-DbaPfDataCollectorSet -CollectorSet 'PAL - SQL Server 2014 and Up' | Invoke-DbaPfRelog -AllowClobber | Select -Expand FullName | Invoke-Item
+Get-DbaPfDataCollectorSet -CollectorSet 'PAL - SQL Server 2014 and Up' | Invoke-DbaPfRelog | Select -Expand FullName | Invoke-Item
 Start-Process C:\temp\perfmon.pbix
 
 
 # Coming soon - shoutout to any Power BI wiz who'd like to help
 Get-DbaPfDataCollectorSet -ComputerName sql2016 -CollectorSet 'Long Running Queries' | Invoke-DbaPfRelog | Update-DbaPowerBiDataSource
-Get-DbaXESession -SqlInstance sql2016 -Session 'Long Running Queries' | Read-DbaXEFile | Update-DbaPowerBiDataSource
+Get-DbaXESession -SqlInstance sql2016 -Session 'Long Running Queries' | Export-DbaXECsv | Update-DbaPowerBiDataSource
 Start-DbaPowerBi
