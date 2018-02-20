1390af0c4810c851e7280b61c6012b4af55e6298
Chrissy LeMaire <clemaire@gmail.com>
2018-02-20 23:28:59 +0100

    updated workload output

--- display-workload.ps1
+++ display-workload.ps1
@@ -2,5 +2,5 @@ $host.ui.RawUI.WindowTitle = "WorkloadReplay Session Watcher"
 Write-Host -Foreground Green "Please go start your workload and come back"
 Import-Module C:\temp\new\dbatools\dbatools.psm1
 
-Get-DbaXESession -SqlInstance localhost\sql2016 -Session WorkloadReplay | Start-DbaXESession | Watch-DbaXESession
+Get-DbaXESession -SqlInstance localhost\sql2016 -Session WorkloadReplay | Start-DbaXESession | Watch-DbaXESession | Select-Object -ExpandProperty batch_text
 
